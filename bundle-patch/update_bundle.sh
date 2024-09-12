ecf565b017b2a50ce1cdbf8198b3689397bba361
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-12 13:40:44 +0000

    Update tempo-gateway to 6a6344f

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -9,7 +9,7 @@ export TEMPO_QUERY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/t
 # Separate due to merge conflicts
 export TEMPO_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-operator@sha256:06b2c5b4081103bc34c53d3a1823eeda3cb24e9a9a17996ce55947c610b347b2"
 # separate due to merge conflicts
-export TEMPO_GATEWAY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-gateway@sha256:5a77802d22e7f1d23279e6e05da98295ef3a07c3f03bbd206b232edf10fe6e8d"
+export TEMPO_GATEWAY_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-gateway@sha256:6a6344f789b3fafaa8ede94a29f3764324e4f3d16ffbca4b02fc4fd1835a9464"
 # separate due to merge conflicts
 export TEMPO_OPA_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-opa@sha256:d4a222f95929c55672263f60e8733d6b23ac9ece42c8723876807007b3856ad0"
 
